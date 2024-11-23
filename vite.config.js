03b701e28ce8833d5b733d8382568b116ec78ab3
homenb1 <hmnbasumatary@gmail.com>
2024-11-23 22:32:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  // Set the base URL for deployment (use the repo name if deploying to GitHub Pages)
+  base: "/shoppingreact", 
   plugins: [react()],
 })
+
